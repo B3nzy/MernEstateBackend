f2b90708c1fd0eaf58485892586a783bb980ed29
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-11-12 13:02:18 +0530

    Connect to database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB Database!");
+  })
+  .catch((err) => {
+    console.log(`${err}`);
+  });
 
 app.listen(PORT, (req, res) => {
   console.log(`Server running on port ${PORT}`);
