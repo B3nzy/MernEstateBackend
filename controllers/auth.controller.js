e0666b67345e52653b5221542b8a73aeb2658dfe
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-11-12 18:44:49 +0530

    Create sign in API route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   try {
@@ -14,8 +15,43 @@ const signup = async (req, res, next) => {
       message: "User created successfully",
     });
   } catch (err) {
-    next(errorHandler(err.statusCode, err.message));
+    next(err);
   }
 };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser.id,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    const { username: usernameDB, email: emailDB } = validUser;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 2 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "Valid user",
+        username: usernameDB,
+        email: emailDB,
+      });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { signup, signin };
