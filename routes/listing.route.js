cd40dd199cdc174a10dc8627db9a0e2f39ebf77c
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-11-20 23:14:40 +0530

    Create get listing API.

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -4,11 +4,13 @@ const {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } = require("../controllers/listing.controller");
 const { verifyToken } = require("../utils/verifyUser");
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.get("/get/:id", verifyToken, getListing);
 router.post("/update/:id", verifyToken, updateListing);
 
 module.exports = router;
